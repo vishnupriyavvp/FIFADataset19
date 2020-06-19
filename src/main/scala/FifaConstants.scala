9377cf114677a1ec8d495feabc0082ffaec6fde8
vishnupriyavvp <vishnupriya258@gmail.com>
2020-06-19 10:37:34 +0530

    init

--- src/main/scala/FifaConstants.scala
+++ src/main/scala/FifaConstants.scala
@@ -1,7 +1,7 @@
 object FifaConstants {
 
   val leftMid = List("LM", "LCM", "LWM")
-  val inputfile="/home/vishnu/Desktop/ACc/data.csv"
+  val inputfile="/home/vishnu/Desktop/ACC/data.csv"
   val PositionList = List("GK","RB","RCB","LCB","LB","RM","LCM","RCM","LM","RF","ST")
   val Hive_table="reporting.fifa19_report_table"
   val url = "jdbc:postgresql://localhost:5432/test_db"
